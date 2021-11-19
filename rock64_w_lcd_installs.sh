071cb5b298eaf6f9b28821a32fa6cff961512529
Robert Rice <rice.robert.t@gmail.com>
2021-11-19 13:03:14 -0800

    Uncommented erroneously commented package installs and group additions

--- rock64_w_lcd_installs.sh
+++ rock64_w_lcd_installs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#sudo apt update && sudo apt upgrade
-#sudo apt install python3-pip
-#sudo pip3 install RPLCD
-#sudo apt install python3-smbus
-#sudo usermod -a -G sudo,i2c $USER
+sudo apt update && sudo apt upgrade
+sudo apt install python3-pip
+sudo pip3 install RPLCD
+sudo apt install python3-smbus
+sudo usermod -a -G sudo,i2c $USER
 echo "* * * * * /home/$USER/rock64_provision/ip2lcd.py" > /home/$USER/rock64_provision/ip2lcd_cronjob
 crontab /home/$USER/rock64_provision/ip2lcd_cronjob
 rm /home/$USER/rock64_provision/ip2lcd_cronjob
